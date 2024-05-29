54dc1c924fd6e51a0de159501a7b18e70732b1ef
Evaggelos Georgiou <evaggelosgeorgiou25@gmail.com>
2024-05-29 21:32:49 +0300

    Go!

---
+++ Hospital.py
@@ -0,0 +1,8 @@
+#We have a patient named John Smith who is 20 years old and is a new patient to the hospital
+patient_first_name = "John"
+patient_last_name = "Smith"
+patient_age = 20
+new_patient = True
+print("Name:",patient_first_name, patient_last_name)
+print("age:",patient_age)
+print("New Patient?", new_patient)
